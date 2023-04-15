9689b3047c395031083474b654ce24e2f91e1dde
Danyal Imran <dan.imran97@gmail.com>
2023-04-15 11:44:31 -0400

    creating functions to both draw and undraw tetrominoes

--- js/app.js
+++ js/app.js
@@ -6,22 +6,57 @@ const width = 10
 
 
 // The Tetrominoes
-const Ltetromino = [
-  [1, width+1, width2+1, 2],
+const lTetromino = [
+  [1, width+1, width*2+1, 2],
   [width, width+1, width+2, width*2+2],
   [1, width+1, width*2+1, width*2],
   [width, width*2, width*2+1, width*2+2]
 ]
 
-const Ttetromino = [
+const tTetromino = [
   [1, width, width+1, width+2],
   [1, width+1, width+2, width*2+1],
   [width, width+1, width+2, width*2+1],
   [1, width, width+1, width*2+1]
 ]
 
-const OTetromino = [
+const oTetromino = [
+  [0, 1, width, width+1],
   [0, 1, width, width+1],
   [0, 1, width, width+1],
   [0, 1, width, width+1]
 ]
+
+const iTetromino = [
+  [1, width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3],
+  [1, width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3]
+]
+
+const allTetrominoes = [lTetromino, tTetromino, oTetromino, iTetromino]
+
+
+let currentPosition = 4
+let currentRotation = 0
+
+// randomly select a Tetromino and its first rotation
+let random = Math.floor(Math.random()*allTetrominoes.length)
+let current = allTetrominoes[random][0]
+
+// draw the first rotation in the first tetromino
+
+function draw() {
+  current.forEach(index => {
+    squares[currentPosition + index].classList.add('tetromino')
+  })
+}
+
+draw()
+
+
+function undraw() {
+  current.forEach(index => {
+    squares[currentPosition + index].classList.remove('tetromino')
+  })
+}
